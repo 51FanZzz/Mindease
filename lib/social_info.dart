a83dcd35065a44bc37c365306bb843548225a177
51FanZzz <163222612+51FanZzz@users.noreply.github.com>
2024-04-24 18:31:00 +1200

    Add files via upload

---
+++ lib/social_info.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class social_info extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Text('social page for Info');
+  }
+  
+}
