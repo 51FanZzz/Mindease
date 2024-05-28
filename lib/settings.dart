145229e5422f5ecd8b10b5e67690bdacee1753a2
51FanZzz <163222612+51FanZzz@users.noreply.github.com>
2024-05-28 15:39:46 +1200

    Add files via upload

---
+++ lib/settings.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class settings extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Text('Settings page. A mock-up for display purpose. Not finalize!!');
+  }
+
+}
