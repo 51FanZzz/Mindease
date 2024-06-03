c805eedda022a04bce979d8f159c3b8516286eed
51FanZzz <163222612+51FanZzz@users.noreply.github.com>
2024-06-04 00:41:30 +1200

    Update daily_entertainment.dart
    update before submission

--- lib/daily_entertainment.dart
+++ lib/daily_entertainment.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class daily_entertainment extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Text('daily_entertainment page');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Entertainment'),
+      ),
+      body: Center(
+        child: Text('Entertainment page'),
+      ),
+    );
+  }
   }
 
-}
