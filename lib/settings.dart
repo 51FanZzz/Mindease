064159dd468385a556a0d33dc1da673b27182636
51FanZzz <163222612+51FanZzz@users.noreply.github.com>
2024-06-04 00:49:04 +1200

    Delete lib/settings.dart
    added new one later

--- lib/settings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class settings extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Text('Settings page. A mock-up for display purpose. Not finalize!!');
-  }
-
-}
