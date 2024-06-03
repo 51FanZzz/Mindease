fa36036d817b196fff6d3dfb36822067001eca6f
51FanZzz <163222612+51FanZzz@users.noreply.github.com>
2024-06-04 00:41:54 +1200

    Update daily_monitor.dart
    update before submission

--- lib/daily_monitor.dart
+++ lib/daily_monitor.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class daily_monitor extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Text('daily_monitor page');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Monitor'),
+      ),
+      body: Center(
+        child: Text('Monitor Page'),
+      ),
+    );
   }
-
-}
+  }
