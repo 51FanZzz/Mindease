bd00adc88da3dbbab2c0597fd42e2882249e6133
51FanZzz <163222612+51FanZzz@users.noreply.github.com>
2024-06-04 00:42:19 +1200

    Update daily_routine.dart
    upodate before submission

--- lib/daily_routine.dart
+++ lib/daily_routine.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class daily_routine extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Text('daily_routine page');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routine'),
+      ),
+      body: Center(
+        child: Text('Routine page'),
+      ),
+    );
   }
 
 }
