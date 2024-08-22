f2275ad6fcc8ee030a8ac670b4d5bfe6361aef45
LunaWu <163222612+51FanZzz@users.noreply.github.com>
2024-08-23 00:29:24 +1200

    RoutineTrackers update
    Now there's picture and number indicators

--- lib/DailyRoutinePage.dart
+++ lib/DailyRoutinePage.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ocd_app/DailyEntertainmentPage.dart';
@@ -23,21 +25,55 @@ class DailyRoutinePage extends StatelessWidget{
               Text(
                 'Schedule Routines', style: TextStyle(color: White, fontSize: titleFontSize, fontWeight: FontWeight.bold),
                  ),
-              
+
+              SizedBox(height: 8,),
+
               _buildRoutineTxt('35', 'minutes you have exercised this week'),
               _buildRoutineTxt('2', '      weeks you have been exercising'),
+
               SizedBox(height: 16,),
-             
+
+              // ignore: sized_box_for_whitespace
+              Container(
+                height: 140,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: 
+                  List.generate(2, (index) {
+                    return Padding(
+                      padding: EdgeInsets.only(right: 8.0),
+                      child: _buildExerciseWidget(
+                        index, 
+                        index == 0 
+                        ? 'assets/images/RopeJumping.png' 
+                        :'assets/images/Meditation.png',
+                        index == 1 
+                        ? 'Try 15 mins of rope jumping' 
+                        :'Try 10 mins of focused meditation',
+                    ),
+                  );
+                }),
+              ),
+            ),
           ],
-            
-          ),
         ),
-        ),
-    );
-  }
+          
+      ),
+        
+    ),
+  );
+}
 
 }
 
+
+
+
+
+
+
+
+// Convinent widget class for Schedule Routines text modifying
 Widget _buildRoutineTxt(String number, String text){
   return Row(
     crossAxisAlignment: CrossAxisAlignment.baseline,
@@ -55,5 +91,38 @@ Widget _buildRoutineTxt(String number, String text){
             ),
        ),
     ],
+  );
+}
+
+// Convinent widget class for routine exercises
+Widget _buildExerciseWidget(int index, String imagePath, String title){
+  Color backgroundColor = index % 2 == 0 ? Colors.lightBlue : Colors.lightGreen;
+
+  return Container(
+    width: 120.0, // the width for the square widgets
+    decoration: BoxDecoration(
+      color: backgroundColor,
+      borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center ,
+        children: [
+          Image.asset(
+            imagePath,
+            width: 70.0,
+            height: 70.0,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 8,),
+          Text(
+            title,
+            style: TextStyle(
+              color:White, fontSize: 5.0
+            ),
+            textAlign: TextAlign.center,
+
+          ),
+        ],
+        ),
   );
 }
