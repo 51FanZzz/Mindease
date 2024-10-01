a9364afd2d1de86fcea775afa57acf99075fc9b7
LunaWu <163222612+51FanZzz@users.noreply.github.com>
2024-10-01 15:43:35 +1300

    Add files via upload

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sheenas/Desktop/Luna docs stuff/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sheenas/Downloads/ocd_app"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\ME\CTEC607\ocd_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
